2fa7a0ccac60d0783322db429a31411b12fa38d9
方洋 <18069898338@163.com>
2021-11-21 16:24:59 +0800

    暂存

---
+++ traceId-commons/traceId-rocketmq-starter/src/test/java/com/example/traceIdRocketmq/TraceIdRocketmqStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traceIdRocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceIdRocketmqStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
