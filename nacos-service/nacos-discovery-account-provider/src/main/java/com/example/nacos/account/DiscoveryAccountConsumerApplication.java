6089dbda2223a7592d504616adbe786284aa17f3
方洋 <18069898338@163.com>
2022-01-05 16:32:51 +0800

    更新服务名称

---
+++ nacos-service/nacos-discovery-account-provider/src/main/java/com/example/nacos/account/DiscoveryAccountConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacos.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryAccountConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryAccountConsumerApplication.class, args);
+    }
+
+}
