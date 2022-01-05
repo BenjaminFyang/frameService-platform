33ba5405e487a13c568e64a095f493dda1b24675
方洋 <18069898338@163.com>
2022-01-05 17:18:19 +0800

    nacos服务 Consumer and Provider 注册

--- nacos-services/nacos-discovery-account-provider/src/main/java/com/example/nacos/account/DiscoveryAccountProviderApplication.java
+++ nacos-services/nacos-discovery-account-provider/src/main/java/com/example/nacos/account/DiscoveryAccountProviderApplication.java
@@ -2,12 +2,14 @@ package com.example.nacos.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class DiscoveryAccountConsumerApplication {
+@EnableDiscoveryClient
+public class DiscoveryAccountProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryAccountConsumerApplication.class, args);
+        SpringApplication.run(DiscoveryAccountProviderApplication.class, args);
     }
 
 }
