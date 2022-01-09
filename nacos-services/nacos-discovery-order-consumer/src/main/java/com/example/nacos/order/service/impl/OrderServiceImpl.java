f422afc4165804bc6cd32a78e1108a50a7c91e57
方洋 <18069898338@163.com>
2022-01-09 17:08:14 +0800

    demo更新

--- nacos-services/nacos-discovery-order-consumer/src/main/java/com/example/nacos/order/service/impl/OrderServiceImpl.java
+++ nacos-services/nacos-discovery-order-consumer/src/main/java/com/example/nacos/order/service/impl/OrderServiceImpl.java
@@ -4,13 +4,11 @@ package com.example.nacos.order.service.impl;
 import com.example.nacos.order.domain.Order;
 import com.example.nacos.order.service.AccountService;
 import com.example.nacos.order.service.OrderService;
-import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * 订单业务实现类
@@ -29,16 +27,11 @@ public class OrderServiceImpl implements OrderService {
      */
     public void create(Order order) {
 
-        List<Order> list = Lists.newArrayList();
-        list.add(order);
-        list.add(order);
-        list.add(order);
-
         LOGGER.info("------->下单开始");
         //本应用创建订单
         LOGGER.info("------->order-service中创建订单");
 
-        //【日志链路追踪】远程调用账户服务扣减余额
+        // 远程调用账户服务扣减余额
         LOGGER.info("------->order-service中扣减余额开始");
         accountService.decrease();
         LOGGER.info("------->order-service中扣减余额结束");
