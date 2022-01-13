fe673e47b26a0dc2c08050c515b49e026909fef5
方洋 <18069898338@163.com>
2022-01-13 19:00:19 +0800

    alibaba:sentinel流量控制实现2.0

---
+++ alibaba-sentinel/src/main/java/com/example/alibaba/sentinel/AlibabaSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.example.alibaba.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaSentinelApplication.class, args);
+    }
+
+}
