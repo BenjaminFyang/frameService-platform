5475c76f96ab4dad4137785619845d9afe3d8b9d
方洋 <18069898338@163.com>
2022-03-28 17:25:31 +0800

    sharding 代码优化

--- sharding-jdbc/src/main/java/com/example/shardingjdbc/service/OrderService.java
+++ sharding-jdbc/src/main/java/com/example/shardingjdbc/service/OrderService.java
@@ -1,16 +1,15 @@
 package com.example.shardingjdbc.service;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.shardingjdbc.dataobject.OrderDO;
 import com.example.shardingjdbc.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 
+@Slf4j
 @Service
 public class OrderService {
 
@@ -19,6 +18,7 @@ public class OrderService {
 
     @Transactional
     public void add(OrderDO order) {
+
         // 这里先假模假样的读取一下。读取从库
         OrderDO exists = orderMapper.selectById(1);
         System.out.println(exists);
@@ -28,19 +28,13 @@ public class OrderService {
 
         // 这里先假模假样的读取一下。读取主库
         exists = orderMapper.selectById(1);
-        System.out.println(exists);
+        log.info("[OrderService][对应的订单数据为order={}]", exists);
     }
 
     public OrderDO findById(Integer id) {
-
         OrderDO order = new OrderDO();
         order.setUserId(20);
         add(order);
-
-        // 数据库的读写分离、分库与分表
-        Page<OrderDO> page = new Page<>(1, 3);
-        IPage<OrderDO> iPage = orderMapper.selectPage(page, null);
-        List<OrderDO> records = iPage.getRecords();
 
         return orderMapper.selectById(id);
     }
