36e654236df8f8dfa6850e10ead590402cda21cb
方洋 <18069898338@163.com>
2022-03-31 16:27:42 +0800

    Hystrix 设置信号量隔离策略

--- netflix-Hystrix/src/main/java/com/example/netflix/hystrix/service/GetProductInfoCommand.java
+++ netflix-Hystrix/src/main/java/com/example/netflix/hystrix/service/GetProductInfoCommand.java
@@ -1,22 +1,37 @@
 package com.example.netflix.hystrix.service;
 
-import com.netflix.hystrix.HystrixCommand;
-import com.netflix.hystrix.HystrixCommandGroupKey;
-import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.*;
 
+
+/**
+ * 可以对接第三方服务接口的限制、
+ */
 public class GetProductInfoCommand extends HystrixCommand<String> {
 
     private final Long cityId;
 
+    private static final HystrixCommandKey KEY = HystrixCommandKey.Factory.asKey("GetCityInfoCommand");
+
     public GetProductInfoCommand(Long cityId) {
+
         // 设置信号量隔离策略
-        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("GetCityNameGroup"))
-                .andCommandPropertiesDefaults(HystrixCommandProperties
-                        .Setter()
-                        .withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.SEMAPHORE)
-                        .withExecutionTimeoutInMilliseconds(1000)
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("CityInfoService"))
+                .andCommandKey(KEY)
+                .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
+                        .withCoreSize(8)
+                        .withMaxQueueSize(10)
+                        .withQueueSizeRejectionThreshold(8))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withCircuitBreakerEnabled(true)
+                        .withCircuitBreakerRequestVolumeThreshold(20)
+                        .withCircuitBreakerErrorThresholdPercentage(40)
+                        .withCircuitBreakerSleepWindowInMilliseconds(3000)
+                        // 设置是否打开超时，默认是true
+                        .withExecutionTimeoutEnabled(true)
+                        // 设置超时时间，默认1000(ms)
+                        .withExecutionTimeoutInMilliseconds(500)
                         // 设置降级机制最大并发请求数
-                        .withFallbackIsolationSemaphoreMaxConcurrentRequests(15)));
+                        .withFallbackIsolationSemaphoreMaxConcurrentRequests(30)));
 
         this.cityId = cityId;
     }
